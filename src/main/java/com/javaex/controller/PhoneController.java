82a47e019005169417b9817618e99e26260f9f94
sojunho <wmswms1020-@naver.com>
2022-06-13 03:38:53 +0900

    phonebook3 복습

---
+++ src/main/java/com/javaex/controller/PhoneController.java
@@ -0,0 +1,92 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.javaex.vo.PersonVo;
+
+@Controller
+/* @RequestMapping(value="/guest") */
+public class PhoneController {
+	
+	//필드
+	
+	
+	
+	//생성자
+	
+	
+	
+	
+	//메소드 gs
+	
+	
+	
+	//메소드 일반
+	//전화번호 등록
+	@RequestMapping(value="/write", method= {RequestMethod.GET, RequestMethod.POST})
+	public String write(@RequestParam("name") String name,
+						@RequestParam("hp") String hp,
+						@RequestParam("company") String company) {
+		System.out.println("write");
+		
+		//파라미터 꺼내기
+		System.out.println(name);
+		System.out.println(hp);
+		System.out.println(company);
+		
+		//vo로 묶기
+		PersonVo personVo = new PersonVo(name, hp, company);
+		System.out.println(personVo);
+		
+		//dao로 저장하기
+		
+		return "";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	//전화번호 등록 폼
+	
+	@RequestMapping(value="/writeForm", method= {RequestMethod.GET, RequestMethod.POST})
+	public String writeForm() {
+		
+		System.out.println("writeForm");
+		return "WEB-INF/views/writeForm.jsp";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//테스트 메소드
+	@RequestMapping(value="/test", method= {RequestMethod.GET, RequestMethod.POST})
+	public String test(){
+		
+		System.out.println("PhoneController>test");
+		//다오
+		return "WEB-INF/views/test.jsp";
+	}
+	
+	
+	
+	
+	
+		//등록 메소드
+		//수정폼 메소드
+		//삭제 메소드
+		//리스트 메소드
+}
